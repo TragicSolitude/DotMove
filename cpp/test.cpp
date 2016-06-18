1d1d90b8249ac5babb9ca7a08a9ede49f81309f2
Noah Shuart <Noah Shuart>
2016-06-17 21:38:08 -0600

    Add execution timing

--- cpp/test.cpp
+++ cpp/test.cpp
@@ -6,9 +6,16 @@
 
 int main(void)
 {
+    std::chrono::time_point<std::chrono::high_resolution_clock> start, end;
+
+    start = std::chrono::high_resolution_clock().now();
     std::array<Dot, DOT_COUNT> dots;
 
     std::cout << dots[0].ToString() << std::endl;
     std::cout << dots[1].ToString() << std::endl;
     std::cout << "Distance Between: " << Dot::DistanceBetween(dots[0], dots[1]) << std::endl;
+    end = std::chrono::high_resolution_clock().now();
+
+    std::cout << "Completed in: " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << "ms" << std::endl;
+    // TODO Calculate average timing
 };
